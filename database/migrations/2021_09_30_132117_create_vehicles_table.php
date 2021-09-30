2f854b8bf911170a2e7a34fc8c7bf7e7de54bc88
aishatpopoola <78761789+aishatpopoola@users.noreply.github.com>
2021-09-30 23:43:46 +0100

    feat: added the migration schema for vehicles table

---
+++ database/migrations/2021_09_30_132117_create_vehicles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->char('vehicle_id')->unique();
+            $table->string('maker');
+            $table->string('model');
+            $table->string('year');
+            $table->string('license_plate')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
