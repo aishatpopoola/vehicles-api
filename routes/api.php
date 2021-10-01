305270d209b1497ba659e73786b55ce41eda6c03
aishatpopoola <78761789+aishatpopoola@users.noreply.github.com>
2021-10-01 13:28:48 +0100

    add vehicle id to delete route

--- routes/api.php
+++ routes/api.php
@@ -11,4 +11,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get("/get-vehicles/{vehicle_id?}", [VehicleController::class, 'getVehicles'])->name('vehicles.get');
 Route::post("/add-vehicle", [VehicleController::class, 'createVehicle'])->name('vehicles.add');
 Route::patch("/update-vehicle", [VehicleController::class, 'updateVehicle'])->name('vehicles.update');
-Route::delete("/delete-vehicle", [VehicleController::class, 'deleteVehicle'])->name('vehicles.delete');
+Route::delete("/delete-vehicle/{vehicle_id}", [VehicleController::class, 'deleteVehicle'])->name('vehicles.delete');
